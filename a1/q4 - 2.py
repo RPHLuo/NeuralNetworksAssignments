9a7c068aeffa6a10a67b24465e8759388e767833
evandaniel15 <35572173+evandaniel15@users.noreply.github.com>
2018-10-07 13:13:45 -0400

    Added functionality to q4
    - Generates results over multiple trials with varying step sizes
    - Results now aggregate and output to a table
    - Starting x vectors are now generated randomly

--- a1/q4 - 2.py
+++ a1/q4 - 2.py
@@ -1,6 +1,7 @@
 import numpy as np
-
-x  = [0.03, 0.02, 0.01]
+import math
+import random
+from astropy.table import Table, Column
 
 A = [[1, 2, 3],
      [2, 3, 4],
@@ -11,17 +12,47 @@ AT = np.transpose(A)
 
 b = [1, 1, 1, 1]
 
-def gradientDescent (x, stepSize, tolerance):
+# These store the values of x and number of iterations over each trial (for the table)
+xList = list()
+countList = list()
+
+# Generates a starting x vector with random numbers normally distributed around the origin
+def generateRandomX():
+    x = list()
+    for i in range (3):
+        x.append(random.normalvariate(0, 2))
+    return x
+
+def gradientDescent (stepSize, tolerance):
+    x = generateRandomX()
+    
+    count = 0
     while (1):
         change = np.matmul(AT, np.matmul(A, x)) - np.matmul(AT, b)
-        if np.linalg.norm(change) <= tolerance: break
-        
-        x = np.subtract(x, stepSize * change)
+        if math.isinf(np.linalg.norm(change, 2)):
+            x = "Could Not Converge (step size too large)"
+            break
+        if np.linalg.norm(change, 2) <= tolerance:
+            break
 
-    print(np.dot(x,A[0]))
-    print(np.dot(x,A[1]))
-    print(np.dot(x,A[2]))
-    print(np.dot(x,A[3]))
-    
-gradientDescent(x, 0.01, 0.01)
+        x = np.subtract(x, stepSize * change)
+        count += 1
         
+    xList.append(str(x))
+    countList.append(str(count))
+
+
+gradientDescent(0.01, 0.01)
+gradientDescent(0.05, 0.01)
+gradientDescent(0.1, 0.01)
+gradientDescent(0.15, 0.01)
+gradientDescent(0.2, 0.01)
+gradientDescent(0.25, 0.01)
+gradientDescent(0.5, 0.01)
+
+
+
+t = Table()
+t['Values of x'] = xList
+t['Number of Iterations'] = countList
+print(t)
