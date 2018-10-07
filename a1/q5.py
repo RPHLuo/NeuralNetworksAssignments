07b720476898a0f48f6436f8b0094fa8c1ffadec
RPHLuo <Robinpluo@gmail.com>
2018-10-07 15:10:32 -0400

    finalized q5

--- a1/q5.py
+++ a1/q5.py
@@ -13,21 +13,23 @@ print(Anull[0])
 print(Anull[1])
 
 Arref = sp.Matrix(A).rref()
+print("Row Echelon form of A:")
 print(Arref)
 # No because the reduced row echelon form indicates
 # that there are only two linearly independent vectors
 
 At = np.transpose(A)
 Atrref = sp.Matrix(At).rref()
+print("Row Echelon form of A transpose:")
 print(Atrref)
 # No because the reduced row echelon form indicates
 # that there are only two linearly independent vectors
-
+print("Null Space:")
 print(linalg.null_space(A))
 # No inverse, there is a pseudoinverse
 invA = np.linalg.pinv(A)
-print("pseudoinverse")
+print("pseudoinverse:")
 print(invA)
 # this kinda shows the identity matrix
-print("A * A pseudoinverse")
+print("A * A pseudoinverse:")
 print(np.matmul(A,invA))
