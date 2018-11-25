92c3ff194f06f54130c979aeeac82b9f724c7f9c
RPHLuo <Robinpluo@gmail.com>
2018-11-25 16:05:16 -0500

    q4

--- a3/question4/q4.py
+++ a3/question4/q4.py
@@ -22,20 +22,20 @@ class FeedForwardNetwork:
                     sess.run(self.train_op, feed_dict={X: trX[start:end], Y: trY[start:end]})
             print(i, sess.run(tf.losses.mean_squared_error(self.teY, sess.run(self.predict_op, feed_dict={X: self.teX}))))
 
-    def __init__(self, shape):
+    def __init__(self, size):
         self.X = tf.placeholder("float", [None, 2])
         self.Y = tf.placeholder("float", [None, 1])
 
         self.size_h1 = tf.constant(size, dtype=tf.int32)
 
-        self.w_h1 = self.init_weights([2, size_h1])
-        self.w_o = self.init_weights([size_h1, 1])
-        self.py_x = model(X, w_h1, w_o)
+        self.w_h1 = self.init_weights([2, self.size_h1])
+        self.w_o = self.init_weights([self.size_h1, 1])
+        self.py_x = model(self.X, self.w_h1, self.w_o)
 
         self.learningRate = 0.02
         self.tolerance = 0.02
-        self.cost = tf.losses.mean_squared_error(py_x, Y)
-        self.train_op = tf.train.MomentumOptimizer(learningRate,0.9).minimize(cost)
+        self.cost = tf.losses.mean_squared_error(self.py_x, self.Y)
+        self.train_op = tf.train.MomentumOptimizer(self.learningRate,0.9).minimize(cost)
         self.predict_op = py_x
 
 lfw_people = fetch_lfw_people(min_faces_per_person=70, resize=0.4)
